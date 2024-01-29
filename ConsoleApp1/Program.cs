7e6303d7df983913904800d221eda52d83b33710
Andrew Forkes <andrew.forkes@ambrosebuilding.com.au>
2024-01-29 13:24:36 +1000

    dataset v1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,8 @@ var configuration = new CsvConfiguration(CultureInfo.InvariantCulture)
     HasHeaderRecord = false,
 };
 
+
+//shouldn't need to change
 using (var reader = new StreamReader("tsa_audit_questions.csv"))
 using (var csv = new CsvReader(reader, configuration))
 {
@@ -22,11 +24,15 @@ using (var csv = new CsvReader(reader, configuration))
     tsaResponses = csv.GetRecords<dynamic>().ToList();
 }
 
+
+
 using (var reader = new StreamReader("Builders_assessment_report_v2.csv"))
 using (var csv = new CsvReader(reader, configuration))
 {
     buildersResponses = csv.GetRecords<dynamic>().ToList();
 }
+
+
 using (var reader = new StreamReader("Builders_assessment_report_tsa_mappings_suncorp.csv"))
 using (var csv = new CsvReader(reader, configuration))
 {
@@ -40,38 +46,42 @@ using (var csv = new CsvReader(reader, configuration))
         var groupId = group.Key;
         var tsaQuestion = getTSAQuestion(groupId);
         var tsaResponse = getTSAResponse(groupId);
-        Console.WriteLine("#Prompt");
-        Console.WriteLine($"Audit Question:{tsaQuestion}"); //Audit question
-        Console.WriteLine();
-        foreach (var record in group) // builder responses that relate to the qudit question
+
+        if (tsaQuestion != null)
         {
-            var builderResponseRow = buildersResponses.Find(x => x.Field5 == record.Field1);
+            Console.WriteLine("#Prompt");
+            Console.WriteLine($"Audit Question:{tsaQuestion}"); //Audit question
+            Console.WriteLine();
+            foreach (var record in group) // builder responses that relate to the qudit question
+            {
+                var builderResponseRow = buildersResponses.Find(x => x.Field5 == record.Field1);
 
-            var builderQuestion = builderResponseRow.Field6;
-            var builderAnswer = builderResponseRow.Field7;
+                var builderQuestion = builderResponseRow.Field6;
+                var builderAnswer = builderResponseRow.Field7;
+                //possible answer options: var builderAnswer2 = builderResponseRow.Field8;
+                Console.WriteLine($"- Q: {builderQuestion} A: {builderAnswer}");
+                // Access the properties of the record as needed
+                var field1 = record.Field1;
+                var field2 = record.Field2;
+            }
+            Console.WriteLine();
+            Console.WriteLine("#Instruction");
+            Console.WriteLine("Review the responses for the audit question, highlight any required responses (*) not provided, ignore others. Where a response is unsatisfactory provide comments about why the response isn't correct and provide details about how the response can be improved.");
 
-            Console.WriteLine($"- Q: {builderQuestion} A: {builderAnswer}");
-            // Access the properties of the record as needed
-            var field1 = record.Field1;
-            var field2 = record.Field2;
+            Console.WriteLine();
+
+            Console.WriteLine("#Output");
+
+            if (tsaResponse != null)
+            {
+                var responseCleaned = tsaResponse.Note.Substring(3, tsaResponse.Note.Length - 3).Trim();
+                Console.WriteLine($"Auditors Note: {responseCleaned} | Auditors Score:{tsaResponse.Score}");
+            }
+            else
+                Console.WriteLine("##No TSA Response##");
+
+            Console.WriteLine();
         }
-        Console.WriteLine();
-        Console.WriteLine("#Instruction");
-        Console.WriteLine("Review the responses for the audit question, highlight any required responses (*) not provided, ignore others. Where a response is unsatisfactory provide comments about why the response isn't correct and provide details about how the response can be improved.");
-
-        Console.WriteLine();
-
-        Console.WriteLine("#Output");
-
-        if (tsaResponse != null)
-        {
-            var responseCleaned = tsaResponse.Note.Substring(3, tsaResponse.Note.Length - 3).Trim();
-            Console.WriteLine($"Auditors Note: {responseCleaned} | Auditors Score:{tsaResponse.Score}");
-        }
-        else
-            Console.WriteLine("##No TSA Response##");
-
-        Console.WriteLine();
     }
 }
 
@@ -79,19 +89,19 @@ string? getTSAQuestion(string field1)
 {
     if (field1 == "N/A")
         return null;
-   var xx = tsaAuditQuestions.First(_ => _.Field1 == field1);
+   var field = tsaAuditQuestions.First(_ => _.Field1 == field1);
 
-    return xx.Field2;
+    return field.Field2;
 }
 
 dynamic? getTSAResponse(string field1)
 {
     if (field1 == "N/A")
         return null;
-    var xx = tsaResponses.First(_ => _.Field1 == field1);
+    var field = tsaResponses.First(_ => _.Field1 == field1);
 
-    var score = xx.Field4;
-    return new { Note = xx.Field2, Score = score };
+    var score = field.Field4;
+    return new { Note = field.Field2, Score = score };
 }
 
 
