a8849dd09ff59f3bb06ac2607c0bcbeb2feb4e18
Andrew Forkes <andrew.forkes@ambrosebuilding.com.au>
2024-01-30 14:53:38 +1000

    Added jsonl output

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,106 +2,162 @@
 using CsvHelper;
 using CsvHelper.Configuration;
 using System.Globalization;
+using System.Text;
+using System.Text.Json.Serialization;
 List<dynamic> tsaAuditQuestions;
-List<dynamic> buildersResponses;
 List<dynamic> tsaResponses;
+string executableDirectory = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+string solutionDirectory = System.IO.Directory.GetParent(executableDirectory).Parent.Parent.FullName;
+
 var configuration = new CsvConfiguration(CultureInfo.InvariantCulture)
 {
     HasHeaderRecord = false,
 };
 
+var tsaAuditQuestionsPath = Path.Combine(solutionDirectory, "tsa_audit_questions.csv");
+var tsaResponsesPath = Path.Combine(solutionDirectory, "datasets", "20015419", "tsa_responses_suncorp.csv");
+var jsonl = Path.Combine(solutionDirectory, "datasets", "20015419", "dataset.jsonl");
+var buildersResponses = Path.Combine(solutionDirectory, "datasets", "20015419", "builders_assessment_report_suncorp.csv");
 
 //shouldn't need to change
-using (var reader = new StreamReader("tsa_audit_questions.csv"))
+using (var reader = new StreamReader(tsaAuditQuestionsPath))
 using (var csv = new CsvReader(reader, configuration))
 {
-   tsaAuditQuestions = csv.GetRecords<dynamic>().ToList();
+    tsaAuditQuestions = csv.GetRecords<dynamic>().ToList();
 }
 
-using (var reader = new StreamReader("Builders_assessment_report_v2_tsa_audit_responses.csv"))
+using (var reader = new StreamReader(tsaResponsesPath))
 using (var csv = new CsvReader(reader, configuration))
 {
     tsaResponses = csv.GetRecords<dynamic>().ToList();
 }
 
-
-
-using (var reader = new StreamReader("Builders_assessment_report_v2.csv"))
-using (var csv = new CsvReader(reader, configuration))
-{
-    buildersResponses = csv.GetRecords<dynamic>().ToList();
-}
-
-
-using (var reader = new StreamReader("Builders_assessment_report_tsa_mappings_suncorp.csv"))
+using (var reader = new StreamReader(buildersResponses))
 using (var csv = new CsvReader(reader, configuration))
 {
     var records = csv.GetRecords<dynamic>();
 
-    var groupedBuilderformresponses = records.GroupBy(_ => _.Field3);
+    var groupedBuilderformresponses = records.GroupBy(_ => _.Field6);
 
+    //UserAgentOutput(tsaAuditQuestions, tsaResponses, groupedBuilderformresponses);
+    JsonlOutput(tsaAuditQuestions, tsaResponses, groupedBuilderformresponses);
+}
+
+string? getTSAQuestion(string tsaQuestionNumber)
+{
+    var field = tsaAuditQuestions.First(_ => _.Field1 == tsaQuestionNumber);
+    return field.Field2;
+}
+
+dynamic? getTSAResponse(string tasResponseNumber)
+{
+    int.TryParse(tasResponseNumber, out var index);
+    var row = tsaResponses[index];
+    var score = row.Field3;
+    return new { Note = row.Field1, Score = score };
+}
+
+void UserAgentOutput(List<dynamic> tsaAuditQuestions, List<dynamic> tsaResponses, IEnumerable<IGrouping<dynamic, dynamic>> groupedBuilderformresponses)
+{
     foreach (var group in groupedBuilderformresponses)
     {
-        Console.WriteLine("------ NEW PROMPT ------");
         var groupId = group.Key;
         var tsaQuestion = getTSAQuestion(groupId);
         var tsaResponse = getTSAResponse(groupId);
 
         if (tsaQuestion != null)
         {
-            Console.WriteLine("#Prompt");
-            Console.WriteLine($"Audit Question:{tsaQuestion}"); //Audit question
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("User:");
+            Console.WriteLine($"Audit Question: {tsaQuestion}"); //Audit question
+            Console.ResetColor(); // Reset the color to the default
             Console.WriteLine();
+            Console.WriteLine("Builder Answers:");
             foreach (var record in group) // builder responses that relate to the qudit question
             {
-                var builderResponseRow = buildersResponses.Find(x => x.Field5 == record.Field1);
-
-                var builderQuestion = builderResponseRow.Field6;
-                var builderAnswer = builderResponseRow.Field7;
-                //possible answer options: var builderAnswer2 = builderResponseRow.Field8;
-                Console.WriteLine($"- Q: {builderQuestion} A: {builderAnswer}");
-                // Access the properties of the record as needed
-                var field1 = record.Field1;
-                var field2 = record.Field2;
+                var builderQuestion = record.Field2;
+                var builderAnswer = record.Field3;
+                Console.WriteLine($"{builderQuestion}: {builderAnswer}");
             }
-            Console.WriteLine();
-            Console.WriteLine("#Instruction");
-            Console.WriteLine("Review the responses for the audit question, highlight any required responses (*) not provided, ignore others. Where a response is unsatisfactory provide comments about why the response isn't correct and provide details about how the response can be improved.");
 
             Console.WriteLine();
-
-            Console.WriteLine("#Output");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Agent:");
 
             if (tsaResponse != null)
             {
                 var responseCleaned = tsaResponse.Note.Substring(3, tsaResponse.Note.Length - 3).Trim();
-                Console.WriteLine($"Auditors Note: {responseCleaned} | Auditors Score:{tsaResponse.Score}");
+                Console.WriteLine($"{responseCleaned} | Score:{tsaResponse.Score}");
             }
             else
                 Console.WriteLine("##No TSA Response##");
-
+            Console.ResetColor(); // Reset the color to the default
             Console.WriteLine();
         }
     }
 }
 
-string? getTSAQuestion(string field1)
+void JsonlOutput(List<dynamic> tsaAuditQuestions, List<dynamic> tsaResponses, IEnumerable<IGrouping<dynamic, dynamic>> groupedBuilderformresponses)
 {
-    if (field1 == "N/A")
-        return null;
-   var field = tsaAuditQuestions.First(_ => _.Field1 == field1);
+    var rows = new List<TrainigRow>();  
 
-    return field.Field2;
+    foreach (var group in groupedBuilderformresponses)
+    {
+
+        var trainingrow = new TrainigRow();
+
+       
+        var groupId = group.Key;
+        var tsaQuestion = getTSAQuestion(groupId);
+        var tsaResponse = getTSAResponse(groupId);
+
+        if (tsaQuestion != null)
+        {
+            var input = new StringBuilder($"Question: {tsaQuestion}");
+            input.AppendLine("Answers:");
+            foreach (var record in group) // builder responses that relate to the qudit question
+            {
+                var builderQuestion = record.Field2;
+                var builderAnswer = record.Field3;
+               input.AppendLine($"{builderQuestion}: {builderAnswer}");
+            }
+            trainingrow.Input = input.ToString();
+
+
+            trainingrow.Instruction = "Review the responses for the audit question, highlight any required responses (*) not provided, ignore others. Where a response is unsatisfactory provide comments about why the response isn't correct and provide details about how the response can be improved.";
+
+
+            var output = new StringBuilder("Agent:");
+
+            if (tsaResponse != null)
+            {
+                var responseCleaned = tsaResponse.Note.Substring(3, tsaResponse.Note.Length - 3).Trim();
+                output.AppendLine($"{responseCleaned} | Score:{tsaResponse.Score}");
+            }
+            else
+                output.AppendLine("##No TSA Response##");
+
+            trainingrow.Output = output.ToString(); 
+            rows.Add(trainingrow);
+        }
+    }
+
+
+    using (StreamWriter writer = new StreamWriter(jsonl))
+    {
+        foreach (var row in rows)
+        {
+            string jsonString = System.Text.Json.JsonSerializer.Serialize(row);
+            Console.WriteLine(jsonString);
+            writer.WriteLine(jsonString);
+        }
+    }
 }
 
-dynamic? getTSAResponse(string field1)
+
+public class TrainigRow
 {
-    if (field1 == "N/A")
-        return null;
-    var field = tsaResponses.First(_ => _.Field1 == field1);
-
-    var score = field.Field4;
-    return new { Note = field.Field2, Score = score };
+    public string Input { get; set; }
+    public string Output { get; set; }
+    public string Instruction { get; set; }
 }
-
-
